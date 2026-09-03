d9af635adde4fa6e0ed8a8c07ee73d7b1366bac1
João Pedro Garcia Torezan <jpgtorezan@gmail.com>
2026-09-03 14:10:13 -0300

    Detalhes que estavam faltando

--- Aulas_JavaScript/Aula_9_DOM/Ex2/main.js
+++ Aulas_JavaScript/Aula_9_DOM/Ex2/main.js
@@ -1,19 +1,22 @@
 const ul = document.querySelector('ul');
 const nome = document.querySelector('#nome');
 const quant = document.querySelector('#quant');
+const vaz = document.querySelector('#vazio');
 const btn = document.querySelector('button');
 
 btn.addEventListener('click', () => {
 
+    vaz.remove();
+
     const newLi = document.createElement('li');
     newLi.innerText = quant.value + " : " + nome.value;
-    
 
     const newButton = document.createElement('button');
     newButton.innerText = 'X';
 
     newButton.addEventListener('click', () => {
         newLi.remove();
+            
     })
 
     newLi.append(newButton);
